40dd5e2c34b6b13ecbdb046415c52abb24871345
Tomas Stolker <stolker@strw.leidenuniv.nl>
2022-04-11 14:14:26 +0200

    v0.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as req_txt:
 
 setuptools.setup(
     name='pycrires',
-    version='0.0.3',
+    version='0.1.0',
     description='Data reduction pipeline for VLT/CRIRES+',
     long_description=open('README.rst').read(),
     long_description_content_type='text/x-rst',
