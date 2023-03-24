96490c19577d51c2be14dd49bfe11c817f4dedae
Rico Landman <ricolandman@Ricos-MacBook-Pro.local>
2023-03-24 13:15:37 +0100

    Add option to load BT-SETTL model:

--- pycrires/util.py
+++ pycrires/util.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.signal import savgol_filter
 from scipy.interpolate import interp1d
+import os
+from PyAstronomy.pyasl import fastRotBroad
 
 def lowpass_filter(flux, window_length):
     nans = np.isnan(flux)
@@ -53,3 +55,71 @@ def flag_outliers(order_spec, sigma=4):
     order_spec[outliers] = np.nan
     return order_spec
 
+
+def load_BT_SETTL_template(temperature, log_g, vsini, wl_lims=[0.5, 3]):
+    import wget
+    import ssl
+    ssl._create_default_https_context = ssl._create_unverified_context
+    #The file names contain the main parameters of the models:
+    #lte{Teff/10}-{Logg}{[M/H]}a[alpha/H].GRIDNAME.7.spec.gz/bz2/xz
+    t_val = int(np.round(temperature/100))
+    log_g_val = 0.5*np.round(log_g/0.5)
+    if t_val<12:
+        fname = 'lte{0:03d}-{1:.1f}-0.0a+0.0.BT-Settl.spec.7.bz2'.format(t_val, log_g_val)
+    else:
+        fname = 'lte{0:03d}.0-{1:.1f}-0.0a+0.0.BT-Settl.spec.7.xz'.format(t_val, log_g_val)
+
+    data_path = os.path.join(os.getcwd(), 'bt_settl_data')
+    if not os.path.exists(data_path):
+        print("Making local data folder...")
+        os.mkdir(data_path)
+    fpath = os.path.join(data_path, fname)
+    decompressed_fpath = fpath[:-4]
+    if not os.path.exists(decompressed_fpath):
+        if t_val<12:
+            url = 'https://phoenix.ens-lyon.fr/Grids/BT-Settl/CIFIST2011/SPECTRA/'+fname
+        else:
+            url = 'https://phoenix.ens-lyon.fr/Grids/BT-Settl/CIFIST2011_2015/SPECTRA/'+fname
+        print('Downloading BT-SETTL spectra from:', url)
+        file = wget.download(url,fpath)
+    
+        if t_val<12:
+            with open(fpath, 'rb') as file:
+                import bz2
+                data = file.read()
+                decompressed_data = bz2.decompress(data)
+        else:
+            import lzma
+            decompressed_data = lzma.open(fpath).read()
+        with open(decompressed_fpath,'wb') as file:
+            file.write(decompressed_data)
+    
+    with open(decompressed_fpath, 'r') as file:
+        data = file.readlines()
+    wl = np.zeros(len(data))
+    flux = np.zeros(len(data))
+
+    for i,line in enumerate(data):
+        split_line = line.split()
+        approx_wl = float(split_line[0][:6])
+        if (approx_wl>wl_lims[0]*1e4)*(approx_wl<wl_lims[1]*1e4):
+            try:
+                wl[i] = float(split_line[0])*1e-4
+                flux[i] = 10**(float(split_line[1].replace('D','E'))-8)
+            except:
+                double_splitted = split_line[0].split('-')
+                wl[i] = float(double_splitted[0])*1e-4
+                if len(split_line)==2:
+                    flux[i] = 10**(float(double_splitted[1].replace('D','E'))-8)
+                else:
+                    flux[i] = 10**(float((double_splitted[1]+'-'+double_splitted[2]).replace('D','E'))-8)
+    if wl_lims is not None:
+        mask = (wl>wl_lims[0])*(wl<wl_lims[1])
+        wl, flux = wl[mask], flux[mask]
+    sorting = np.argsort(wl)
+    wl = wl[sorting]
+    flux = flux[sorting]
+    waves_even = np.linspace(np.min(wl), np.max(wl), wl.size*2)
+    flux_even = interp1d(wl, flux)(waves_even)
+    broad_flux = fastRotBroad(waves_even, flux_even, 0.5, vsini)
+    return broad_flux, waves_even*1000
