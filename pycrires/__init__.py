104ebb065eaf171128e1ff4c0da82bb5478f7caf
Tomas Stolker <stolker@strw.leidenuniv.nl>
2024-06-28 09:00:52 +0200

    v0.3.1

--- pycrires/__init__.py
+++ pycrires/__init__.py
@@ -3,7 +3,7 @@ from pycrires.util import load_bt_settl_template
 
 __author__ = 'Tomas Stolker'
 __license__ = 'MIT'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __maintainer__ = 'Tomas Stolker'
 __email__ = 'stolker@strw.leidenuniv.nl'
 __status__ = 'Development'
